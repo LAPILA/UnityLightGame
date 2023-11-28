30731b20230db5e913cd28390650131b1f901ebb
monokatare <hhpaul1234@gmail.com>
2023-11-28 14:35:19 +0900

    MosterChest Animation

--- unity_light_game/Assets/2_Scripts/Monster/MonsterChest.cs
+++ unity_light_game/Assets/2_Scripts/Monster/MonsterChest.cs
@@ -7,7 +7,6 @@ public class MonsterChest : MonoBehaviour
     private bool Interacted=false;
     private int count = 0;
     private float time = 0;
-    private bool Isopen = false;
     private Animator animator;
     void Start()
     {
@@ -27,15 +26,28 @@ public class MonsterChest : MonoBehaviour
             animator.SetBool("IsATT", Interacted);
             count = 1;
         }
-        else if (Interacted) {
+        else if (Interacted && count == 1)
+        {
             time += Time.deltaTime;
             Debug.Log(time);
+            if (time > 1.45f)
+            {
+                animator.SetInteger("motionT", 10);
+                count = 10;
             }
+        }
+        else if (count == 10) {
+
+            count = 0;
+            time = 0;
+            Interacted = false;
+            animator.SetBool("IsATT", Interacted);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        TransUpdate();
     }
 }
